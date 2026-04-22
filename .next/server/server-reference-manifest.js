393f20ca46769cc60d278007680dc479baa94924
Anshu Shetty <anshushetty2003@gmail.com>
2026-04-22 14:22:02 +0530

    sitemap.xml added

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,5 @@
+<<<<<<< Updated upstream
 self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"AJDfhNXnVAafndyq8lYEgLgbBdI+7CHW4d1d/kQg3R4=\"}"
+=======
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"9L56FgAIeGFn9YD5Tnmupni9aeSR8t8iDSBtwIAaBJE=\"\n}"
+>>>>>>> Stashed changes
